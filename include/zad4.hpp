df5304583d1e92468e77033697c8f0f411224218
rkawczak97 <rkawczak97@gmail.com>
2021-01-15 08:43:18 +0000

    Zad4 v1

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -4,3 +4,14 @@
 #include <vector>
 
 // tutaj funkcja stworzZespol
+stworzZespol(double a, std::string s){
+  if(size(s) != a){
+    std::logic_error = "error";
+    throw(logic_error);
+  }
+  std::vector<Artysta> v(a);
+  v.push_back(Artysta(s, 1));
+  v.push_back(Artysta(s, 2)); 
+  v.push_back(Artysta(s, 3));
+  return v;  
+}

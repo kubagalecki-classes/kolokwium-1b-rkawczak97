e6ee3cf22832d8855d0dcc25c2b51869c7dbbd06
rkawczak97 <rkawczak97@gmail.com>
2021-01-15 07:41:23 +0000

    Zad v5

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -6,15 +6,15 @@
 #include <sstream>
 #include <string>
 
+using namespace std;
 // tutaj klasa Artysta
 class Artysta {
 	Artysta(){
     this-> pseudonim = "Artysta";
     this-> liczba_sluchaczy = 0;
-  };
-	Artysta(const std::string& _psedonim, unsigned _liczba_sluchaczy) :
-		pseudonim(_psedonim), liczba_sluchaczy(_liczba_sluchaczy) {};
-	~Artysta() {};
+  }
+	Artysta(const std::string& _psedonim, unsigned _liczba_sluchaczy) : pseudonim(_psedonim), liczba_sluchaczy(_liczba_sluchaczy) {}
+	~Artysta() {}
 
 	void graj(std::ostream& os) const {
 		os << this->pseudonim << ": " << this->liczba_sluchaczy;
@@ -27,3 +27,4 @@ private:
 	std::string pseudonim;
 	unsigned liczba_sluchaczy;
 };
+

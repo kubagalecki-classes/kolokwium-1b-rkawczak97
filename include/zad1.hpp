139cff25c0063447789102cb7708d27138074188
rkawczak97 <rkawczak97@gmail.com>
2021-01-15 07:29:47 +0000

    test

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -7,3 +7,20 @@
 #include <string>
 
 // tutaj klasa Artysta
+class Artysta {
+	Artysta() : pseudonim("Anonim") {};
+	Artysta(const std::string& _psedonim, unsigned _liczba_sluchaczy) :
+		pseudonim(_psedonim), liczba_sluchaczy(_liczba_sluchaczy) {};
+	~Artysta() {};
+
+	void graj(std::ostream& os) const {
+		os << this->pseudonim << " : " << this->liczba_sluchaczy;
+	}
+
+	const std::string& getPseudonim() const { return pseudonim; }
+	unsigned getSluchacze() const { return liczba_sluchaczy; }
+
+private:
+	std::string pseudonim;
+	unsigned liczba_sluchaczy;
+};

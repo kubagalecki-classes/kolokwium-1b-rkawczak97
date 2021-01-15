6b3b46cb2f0a06b1580ddef803dab5dcaa4a2256
rkawczak97 <rkawczak97@gmail.com>
2021-01-15 08:23:22 +0000

    Zad3 v1

--- include/zad3.hpp
+++ include/zad3.hpp
@@ -3,3 +3,12 @@
 #include "zad1.hpp"
 
 //tutaj szablon ArtystaPlusInstrument
+template<class T>
+class ArtystaPlusInstrument {
+public:
+	ArtystaPlusInstrument(const Artysta& A, const& T t) {}
+	unsigned koncert(){ return i.liczbaUczestnikow(a); }
+private:
+	Artysta a;
+	T i;
+};

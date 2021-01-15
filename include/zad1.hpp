4456593408411eb18b095978390a2fa7866d73bb
rkawczak97 <rkawczak97@gmail.com>
2021-01-15 07:43:52 +0000

    Zad v6

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -8,23 +8,24 @@
 
 using namespace std;
 // tutaj klasa Artysta
-class Artysta {
-	Artysta(){
-    this-> pseudonim = "Artysta";
-    this-> liczba_sluchaczy = 0;
-  }
-	Artysta(const std::string& _psedonim, unsigned _liczba_sluchaczy) : pseudonim(_psedonim), liczba_sluchaczy(_liczba_sluchaczy) {}
-	~Artysta() {}
-
-	void graj(std::ostream& os) const {
-		os << this->pseudonim << ": " << this->liczba_sluchaczy;
-	}
-
-	const std::string& getPseudonim() const { return this->pseudonim; }
-	unsigned getSluchacze() const { return this->liczba_sluchaczy; }
-
+class Artysta 
+{
+public:
+    Artysta() { this->pseudonim = "Anonim"; this->liczba_sluchaczy = 0; }
+    Artysta(const std::string& p, unsigned l) : pseudonim(p), liczba_sluchaczy(l) {}
+    void graj(std::ostream& os) const {
+        os << this->pseudonim << ": " << this->liczba_sluchaczy;
+    }
+    const std::string& getPseudonim() const
+    {
+        return this->pseudonim;
+    }
+    unsigned getSluchacze() const
+    {
+        return this->liczba_sluchaczy;
+    }
 private:
-	std::string pseudonim;
-	unsigned liczba_sluchaczy;
+    string pseudonim;
+    unsigned liczba_sluchaczy;
 };
 
